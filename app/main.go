02f20c3b8c9057e29a50d034864fc83725c97bc8
wingkwong <wingkwong.code@gmail.com>
2020-03-30 21:22:23 +0800

    Create main.go

---
+++ app/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", Handler)
+
+	if err := http.ListenAndServe(":8888", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	io.WriteString(w, `{"status":"ok"}`)
+}
